7a379aa3804070636db0fa648b0f7327bcef4b4e
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-07-10 12:14:11 +0200

    Adding app architecture

---
+++ IchLerneDeutsch.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IchLerneDeutsch.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
